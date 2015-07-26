6b5a72aff485759fb4ed5b6cd35b749e22922d43
Bobjrsenior <bobjrsenior@gmail.com>
2015-07-25 23:33:50 -0400

    Can now replace in python mosty correctly. Need to get rid of extra \ characters before saving

--- imageSwapper.py
+++ imageSwapper.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import time
+import random
 import praw
 import re
 import datetime
@@ -12,7 +13,7 @@ def timeTil(): #Returns a formatted length until the event
      minutes = int(td.seconds / 60) - (hours * 60) 
      return keyword + ': ' + str(days) + 'd:' + str(hours) + 'h:' + str(minutes) + 'm' + endKeyword
 
-user_agent = ("Header Image Swappexr v0.0.1 by /u/bobjrsenior") #Let reddit know who you are
+user_agent = ("Header Image Swappexr v0.0.5 by /u/bobjrsenior") #Let reddit know who you are
 r = praw.Reddit(user_agent=user_agent)
 r.login(input('Username: '), getpass.getpass()) #Input username, pass here
 r.config.decode_html_entities = True
@@ -20,8 +21,6 @@ subredditName = input('Subreddit Name: ') #Name of the subreddit
 subreddit = r.get_subreddit(subredditName)  #get the subreddit
 updateSpeed = 60 #Update the timer every _ seconds
 stylesheet = subreddit.get_stylesheet()['stylesheet']
-pprint(stylesheet)
-time.sleep(1800)
 
 #get inputs for dates and such
 keyword = input('Keyword in sidebar to find: ')
@@ -29,14 +28,15 @@ endKeyword = input('Keyword that marks the end of the countdown (\\n if new line
 images = []
 imageNameTemp = input('Image Name (q for done): ')
 while (imageNameTemp != 'q'):
-    images += imageNameTemp
+    images.append(imageNameTemp)
+    imageNameTemp = input('Image Name (q for done): ')
 
 while True: #Infinite Loop since it is a bot
     settings = r.get_settings(subredditName) #Get the subreddits settings
-    description = settings['description'] #Get the sidebar
-    #Update the countdown in the sidebar
-    description = re.sub(keyword + ':.*' + endKeyword, images[random.randint(0, images.len())], description, 1)
-    #Update the subreddit
-    r.update_settings(subreddit , description=description)
+    #Update the local css with a new image
+    stylesheet = re.sub(keyword + '.*' + endKeyword, keyword + '/background:url(%%' + images[random.randint(0, len(images) - 1)] + '%%)'+ endKeyword, stylesheet, 1)
+    #Update the sub's css
+    pprint(stylesheet)
+    subreddit.set_stylesheet(stylesheet)
     #Sleep until we need to update again
     time.sleep(updateSpeed)
