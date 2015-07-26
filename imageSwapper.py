4231dceda75e7850a8b6f5aab6ee17ace5375f55
Bobjrsenior <bobjrsenior@gmail.com>
2015-07-26 12:19:08 -0400

    Got it mostly working. I can successfully replace the css and save it without syntax errors. Now I need to do testing for delayed changes and such.

--- imageSwapper.py
+++ imageSwapper.py
@@ -3,40 +3,39 @@ import time
 import random
 import praw
 import re
-import datetime
 import getpass
 
-def timeTil(): #Returns a formatted length until the event
-     td = datetime.datetime(year, month, day, hour, minute, 0) - datetime.datetime.now()
-     days = td.days
-     hours = int(td.seconds / 3600)
-     minutes = int(td.seconds / 60) - (hours * 60) 
-     return keyword + ': ' + str(days) + 'd:' + str(hours) + 'h:' + str(minutes) + 'm' + endKeyword
-
-user_agent = ("Header Image Swappexr v0.0.5 by /u/bobjrsenior") #Let reddit know who you are
+user_agent = ("Header Image Swappexr v0.0.7 by /u/bobjrsenior") #Let reddit know who you are
 r = praw.Reddit(user_agent=user_agent)
 r.login(input('Username: '), getpass.getpass()) #Input username, pass here
 r.config.decode_html_entities = True
 subredditName = input('Subreddit Name: ') #Name of the subreddit
 subreddit = r.get_subreddit(subredditName)  #get the subreddit
-updateSpeed = 60 #Update the timer every _ seconds
-stylesheet = subreddit.get_stylesheet()['stylesheet']
+stylesheet = subreddit.get_stylesheet()['stylesheet'] #get the css
 
-#get inputs for dates and such
-keyword = input('Keyword in sidebar to find: ')
+#get The location of the css to replace
+startKeyword = input('Keyword in sidebar to find: ')
 endKeyword = input('Keyword that marks the end of the countdown (\\n if new line): ')
+
+#Make keywords more regex compatable
+startKeywordSan = re.sub('\*', '\*', startKeyword)
+endKeywordSan = re.sub('\*', '\*', endKeyword)
+
+#get the images in the cycle
 images = []
 imageNameTemp = input('Image Name (q for done): ')
 while (imageNameTemp != 'q'):
     images.append(imageNameTemp)
     imageNameTemp = input('Image Name (q for done): ')
 
+#get the delat between image changes
+timeDelay = input('Time between images (minutes): ')
+timeDelay *= 60
 while True: #Infinite Loop since it is a bot
     settings = r.get_settings(subredditName) #Get the subreddits settings
     #Update the local css with a new image
-    stylesheet = re.sub(keyword + '.*' + endKeyword, keyword + '/background:url(%%' + images[random.randint(0, len(images) - 1)] + '%%)'+ endKeyword, stylesheet, 1)
+    stylesheet = re.sub(startKeywordSan + '.*' + endKeywordSan, startKeyword + 'background:url(%%' + images[random.randint(0, len(images) - 1)] + '%%) no-repeat top left;'+ endKeyword, stylesheet, 1)
     #Update the sub's css
-    pprint(stylesheet)
     subreddit.set_stylesheet(stylesheet)
     #Sleep until we need to update again
-    time.sleep(updateSpeed)
+    time.sleep(timeDelay)
