7f8c24edf00bbd8c04fb7a81436588130c4824e2
Bobjrsenior <bobjrsenior@gmail.com>
2015-07-26 12:41:15 -0400

    Added a cycle count. Fixed wording of the endKeyword. Remembered to typecaste timeDelay to an int. It is working and updating every 15 minutes now. Next thing will be to use a non-deprecated log-in system.

--- imageSwapper.py
+++ imageSwapper.py
@@ -5,7 +5,7 @@ import praw
 import re
 import getpass
 
-user_agent = ("Header Image Swappexr v0.0.7 by /u/bobjrsenior") #Let reddit know who you are
+user_agent = ("Header Image Swappexr v0.1.0 by /u/bobjrsenior") #Let reddit know who you are
 r = praw.Reddit(user_agent=user_agent)
 r.login(input('Username: '), getpass.getpass()) #Input username, pass here
 r.config.decode_html_entities = True
@@ -15,7 +15,7 @@ stylesheet = subreddit.get_stylesheet()['stylesheet'] #get the css
 
 #get The location of the css to replace
 startKeyword = input('Keyword in sidebar to find: ')
-endKeyword = input('Keyword that marks the end of the countdown (\\n if new line): ')
+endKeyword = input('Keyword that marks the end of the replacement (\\n if new line): ')
 
 #Make keywords more regex compatable
 startKeywordSan = re.sub('\*', '\*', startKeyword)
@@ -29,13 +29,16 @@ while (imageNameTemp != 'q'):
     imageNameTemp = input('Image Name (q for done): ')
 
 #get the delat between image changes
-timeDelay = input('Time between images (minutes): ')
+timeDelay = int(input('Time between images (minutes): '))
 timeDelay *= 60
+cycles = 0
 while True: #Infinite Loop since it is a bot
+    cycles += 1
     settings = r.get_settings(subredditName) #Get the subreddits settings
     #Update the local css with a new image
     stylesheet = re.sub(startKeywordSan + '.*' + endKeywordSan, startKeyword + 'background:url(%%' + images[random.randint(0, len(images) - 1)] + '%%) no-repeat top left;'+ endKeyword, stylesheet, 1)
     #Update the sub's css
     subreddit.set_stylesheet(stylesheet)
+    pprint(cycles)
     #Sleep until we need to update again
     time.sleep(timeDelay)
